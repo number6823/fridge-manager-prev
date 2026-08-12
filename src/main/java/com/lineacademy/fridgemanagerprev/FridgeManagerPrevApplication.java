dc6850c13a38953cc4542f581c613f24b165fca0
number6823 <ohdehet@naver.com>
2026-08-12 14:25:27 +0900

    초기화

---
+++ src/main/java/com/lineacademy/fridgemanagerprev/FridgeManagerPrevApplication.java
@@ -0,0 +1,13 @@
+package com.lineacademy.fridgemanagerprev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeManagerPrevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgeManagerPrevApplication.class, args);
+    }
+
+}
